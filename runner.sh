dda922ff915bd3a1be49a0eee789bb7dad09714d
Divyanshu Rajput <dvnhrajput@gmail.com>
2025-05-30 14:52:01 +0530

    Model retrained!

--- runner.sh
+++ runner.sh
@@ -1,9 +1,13 @@
+echo "Running pipeline..."
 dvc repro --force
 
 git add .
 
+echo "Pushing results..."
 dvc push
 
 git commit -m "Model retrained!"
 
+echo "Pushing to github..."
 git push
+echo "All done"
